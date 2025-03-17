b8e2ce51c9744a5c03e0fb3cf5e8a5a71338ecb2
NeiJunio <neijunio50@gmail.com>
2025-03-17 12:41:29 -0300

    commit

---
+++ Petfolio.API/Controllers/PetController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pet.Register;
+using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PetController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
+    public IActionResult Register([FromBody] RequestRegisterPetJson request)
+    {
+        var useCase = new RegisterPetUseCase(); // Cria uma instância direcionada para a classe RegisterPetUseCase
+        
+        var response = useCase.Execute(request); // dentro da classe RegisterPetUseCase, acessa a função com nome Execute(), passando para ela o parâmetro request
+
+        return Created(string.Empty, response); // a função Created sempre retorna dois parâmetros, então para evitar conflitos, sempre passar o primeiro parâmetro como strng.Empty
+    }
+}
---
+++ Petfolio.Communication/Requests/RequestRegisterPetJson.cs
@@ -0,0 +1,9 @@
+﻿using Petfolio.Communication.Enums;
+
+namespace Petfolio.Communication.Requests;
+public class RequestRegisterPetJson
+{
+    public string Name { get; set; } = string.Empty; // Nome nunca vai ser nulo, no máximo uma string vazia
+    public DateTime Birthday { get; set; }
+    public PetType Type { get; set; }
+}
